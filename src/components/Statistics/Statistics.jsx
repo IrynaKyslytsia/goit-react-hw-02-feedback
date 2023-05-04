da00212532c60715104f0cfbf34aaa5d74d1e5b2
IrynaKyslytsia <i.kislica89@gmail.com>
2023-05-04 21:41:11 +0300

    Add Notification

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Notification from 'components/Notification/Notification';
 import { StatList, StatItem } from './Statistics.styled';
 
 const Statistics = ({good, neutral, bad, total = 0, positiveFeedbackPercentage = 0}) => 
 {
-    return (
+    return total ? (
         <StatList>
         <StatItem>Good: {good}</StatItem>
         <StatItem>Neutral: {neutral}</StatItem>
@@ -12,7 +13,7 @@ const Statistics = ({good, neutral, bad, total = 0, positiveFeedbackPercentage =
         <StatItem>Total: {total}</StatItem>
         <StatItem>Positive feedback: {positiveFeedbackPercentage ? positiveFeedbackPercentage : 0}%</StatItem>
         </StatList>
-    )
+    ) : <Notification message='There is no feedback'/>
 };
 
 export default Statistics;
